248032bd19bf64c9610e161531483c8e3eb0b609
Murat Amin <Murat_94_124@hotmail.com>
2019-09-16 11:32:38 +0200

    Hello GitHub

--- test7/Program.cs
+++ test7/Program.cs
@@ -10,7 +10,7 @@ namespace test7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello GitHub");
             Console.ReadLine(); 
 
 
